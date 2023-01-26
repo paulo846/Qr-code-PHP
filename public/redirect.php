870f108f92ac687746c48e8f2aa0d9019abf7bec
Paulo Henrique Duarte <igrsysten@gmail.com>
2023-01-26 20:14:08 -0300

    Atualiza função de redirecionamento

--- public/redirect.php
+++ public/redirect.php
@@ -34,7 +34,7 @@ endif ;
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <?php if (isset($_GET['site'])) : ?>
-        <meta http-equiv="refresh" content="10; url=<?= $_GET['site'] ?>">
+        <meta http-equiv="refresh" content="5; url=<?= $_GET['site'] ?>">
     <?php endif; ?>
 
     <link rel="stylesheet" href="assets/css/style.min.css">
@@ -65,7 +65,7 @@ endif ;
             var countDownDate = new Date().getTime() + 10000; //10 segundos
             var redirectUrl = "<?= $_GET['site'] ?>"; // URL para redirecionamento
         </script>
-        <script src="assets/js/redirect.min.js"></script>
+        <script src="assets/js/redirect.min.js?time=<?= uniqid() ?>"></script>
     <?php endif; ?>
 </body>
 
